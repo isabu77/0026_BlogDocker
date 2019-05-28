7257d214d7994649a1f2b5c415a83c8b8b8097f9
isabu77 <isabu77@gmail.com>
2019-05-28 16:52:14 +0200

    V1.1.2 correction Pagination

--- home.php
+++ home.php
@@ -1,10 +1,34 @@
 <?php
     $pdo = new PDO('mysql:dbname=blog;host=172.17.0.2;charset=UTF8', 'bloguser', 'blogpwd');
-	$sql = "SELECT * FROM `post`";
-	$statement = $pdo->prepare($sql);
-	$statement->execute(); 
-	$posts = $statement->fetchAll();
+	// $sql = "SELECT COUNT(id) FROM `post`";
+	// $statement = $pdo->prepare($sql);
+    // $statement->execute(); 
 
+    // nb de pages : nb total / 10
+    $pagination = $pdo->query('SELECT count(id) FROM `post`')->fetch()[0] /10;
+    //$pagination = $pdo->query('SELECT count(id) as nbid FROM `post`')->fetch() /10;
+    
+    if (null != $_GET['page'] && intval($_GET['page']) > 0 && $_GET['page'] <= $pagination){
+        $start = 10 * $_GET['page'] - 10;
+    }else{
+            if (null != $_GET['page'] && !intval($_GET['page']) || $_GET['page'] > $pagination){
+                $message = "page introuvable";
+            }
+            $start = 0;
+        }
+
+        // liste des 10 posts de la page choisie :
+        // $sql = "SELECT * FROM `post` ORDER BY `id` LIMIT 10 OFFSET " . 
+        //          ( ($_GET['page'] == 1)? (0) : (($_GET['page']-1)*10)) ;
+        $sql = "SELECT * FROM post ORDER BY id LIMIT 10 OFFSET {$start}" ; 
+
+        $statement = $pdo->prepare($sql);
+        $statement->execute(); 
+        $posts = $statement->fetchAll(); // ici renvoie un tableau 
+        //$rec = $statement->fetchAll(PDO::FETCH_OBJ); // PDO::FETCH_OBJ pour avoir un objet
+    // }else{
+    //     $posts = [];
+    // }
 ?>
 <html>
 <!DOCTYPE html>
@@ -23,33 +47,29 @@
         <h1>Le BLOG Docker</h1>
     </section>
     <?php 
-    if ($_GET['page'] < 1 || $_GET['page'] > 5){
-        echo '<h2>page inconnue</h2>';
+    if (count($posts) <= 0){
+        echo '<h2>page introuvable</h2>';
     }else{?>
     <section id="posts">
-    <?php 
-   for($i = 10 * ($_GET['page']-1) ; $i < 10 * $_GET['page'] ; $i++) { 
-
-    ?>
+        <?php 
+   //for($i = 10 * ($_GET['page']-1) ; $i < 10 * $_GET['page'] ; $i++) { 
+   // for($i = 0 ; $i < count($posts) ; $i++) { 
+       foreach($posts as $post) : ?>
         <article class="post">
-            <p class="titre"><?= $posts[$i]['id'] . '-' . $posts[$i]['name']; ?></p>
-            <p class="slug"><?= $posts[$i]['slug']; ?></p>
-            <p class="content"><?= substr((String)$posts[$i]['content'],0,150) . '...'; ?></p>
+            <p class="titre"><?= $post['id'] . '-' . $post['name']; ?></p>
+            <p class="slug"><?= $post['slug']; ?></p>
+            <p class="content"><?= substr((String)$post['content'],0,150) . '...'; ?></p>
         </article>
-        <?php } }?>
+        <?php endforeach ?>
     </section>
-
+    <?php } ?>
     <footer>
         <div>
             <nav>
                 <ul>
-                    <?php
-                    for ($i = 0 ; $i < count($posts); $i += 10){
-                     ?>
-                        <li><a href="?page=<?= $i/10+1 ?>"><?= $i/10+1 ?></a></li>
-                    <?php
-                    }
-                    ?>
+                    <?php for ($i = 1 ; $i <= $pagination; $i++) :  ?>
+                    <li><a href="/?page=<?= $i ?>"><?= $i ?></a></li>
+                    <?php endfor ?>
                 </ul>
             </nav>
         </div>
