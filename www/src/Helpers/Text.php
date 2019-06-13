1005dc1a50036eab1cd95a4fc4731076de69d2f9
isabu77 <isabu77@gmail.com>
2019-06-13 13:16:30 +0200

    TESTS de Text::Excerpt()

--- www/src/Helpers/Text.php
+++ www/src/Helpers/Text.php
@@ -19,11 +19,24 @@ class Text
      **/
     public static function excerpt(string $content, int $limit = 100): string
     {
+        // pour oter les balise html :
+        $content = strip_tags($content);
+
+        // si la chaine est plus petite que la limite, on la rend entière
         if (mb_strlen($content) <= $limit) {
             return $content;
         }
-        // limite + un mot pour ne pas couper le dernier mot
-        $lastSpace = mb_strpos($content, ' ', $limit);
+
+        // pour ne pas couper le dernier mot, on cherche le premier espace derrière la limite
+        // $limit-1 pour gérer le cas d'un espace en position 100
+        $lastSpace = mb_strpos($content, ' ', $limit-1);
+
+        // cas d'une chaine sans espaces :
+        if ($lastSpace == null)
+            return mb_substr($content, 0, 36) . '...';
+
+        // autres cas : on tronque à la limite et ajout des '...'
         return mb_substr($content, 0, $lastSpace) . '...';
+        
     }
 }
